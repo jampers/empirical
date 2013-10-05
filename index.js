f1a606df963c30468edf5089d5f5836149e65f52
jampers <user@server.fake>
2013-10-05 09:08:50 +0100

    code: bare bones

---
+++ index.js
@@ -0,0 +1,3 @@
+var empirical = {};
+
+module.exports = empirical;
---
+++ test/empirical-tests.js
@@ -0,0 +1,8 @@
+var empirical = require('../')
+	, test = require('tape');
+
+test('empirical exists', function (assert) {
+	assert.ok(!!empirical);
+	assert.end();
+});
+
